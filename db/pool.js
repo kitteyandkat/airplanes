7e07ba19c16b32928e034a558923a5979c7725fe
Sarah Johnson <kitteyandkat@aim.com>
2023-04-15 10:37:05 -0400

    deployment fixes

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "containers-us-west-54.railway.app",
-  password:'VeBQ97WflQPRD6DlsNdc',
-  database: "airplanes",
-  port: 5457
+  // user: "postgres",
+  // host: "containers-us-west-54.railway.app",
+  // password:'VeBQ97WflQPRD6DlsNdc',
+  // database: "airplanes",
+  // port: 5457
+  database: process.env.DATABASE_URL
 })       
 
 module.exports = () => pool;
+// pool.query(`CREATE DATABASE airplanes`)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { create, read, update, remove }  = require('./db/index.js');
+require('dotenv').config()
 
-const port = 3001;
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
